6302ffd31889af5a3bfcf34bb06662e8560a9dee
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2023-06-10 13:31:31 +0530

    with transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -23,26 +23,71 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
+    def get_iqr(self,data, column_name, q1_range, q3_range):
+        q1 = data[column_name].quantile(q1_range)
+        q3 = data[column_name].quantile(q3_range)
+        IQR = q3 - q1
+        upper_fence = q3 + (1.5 * IQR)
+        lower_fence = q1 - (1.5 * IQR)
+
+
+        indexUL = data[data[column_name] > upper_fence].index
+        indexLM = data[data[column_name] < lower_fence].index
+
+        # print("indexUL: "+str(indexUL))
+        # print("indexLM: "+str(indexUL))
+
+        print(data.shape)
+
+        data.drop(indexUL, inplace=True)
+        data.drop(indexLM, inplace=True)
+        print(data.shape)
+        return data
+
     def initiate_data_ingestion(self):
+
         try:
-            df=pd.read_csv('notebook/data/tourismData.csv')
+
+            data=pd.read_csv('notebook/data/tourismData.csv')
+            data.drop(['CustomerID'], axis=1,inplace=True)
+
+            data['Age'].fillna(data['Age'].median(), inplace=True)
+            data['TypeofContact'].fillna(data['TypeofContact'].mode()[0], inplace=True)
+            data['DurationOfPitch'].fillna(data['DurationOfPitch'].median(), inplace=True)
+            data['NumberOfFollowups'].fillna(data['NumberOfFollowups'].median(), inplace=True)
+            data['PreferredPropertyStar'].fillna(data['PreferredPropertyStar'].median(), inplace=True)
+            data['NumberOfTrips'].fillna(data['NumberOfTrips'].median(), inplace=True)
+            data['NumberOfChildrenVisited'].fillna(data['NumberOfChildrenVisited'].median(), inplace=True)
+            data['MonthlyIncome'].fillna(data['MonthlyIncome'].median(), inplace=True)
+
+            data = self.get_iqr(data, 'MonthlyIncome',0.20,0.90)
+            data = self.get_iqr(data, 'DurationOfPitch',0.10,0.85)
+            data = self.get_iqr(data, 'NumberOfTrips',0.20,0.75)
+            data = data
+
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
-            train_set.to_csv(self.ingestion_config.test_data_path,index=False)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_set,test_set = train_test_split(data,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False)
+
 
             print("files step-1")
+
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.test_data_path,
+
             )
+
             print("files step-2")
+
         except Exception as e:
             raise CustomException(e,sys)
 
 
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path,test_path=obj.initiate_data_ingestion()
 
 
---
+++ src/components/data_transformation.py
@@ -0,0 +1,64 @@
+import sys
+from dataclasses import dataclass
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+
+from src.exception import CustomException
+from src.utils import save_object
+
+from src.logger import logging
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def initiate_data_transformation(self,train_path,test_path):
+
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+            logging.info("Obtaining preprocessing object")
+            target_column_name = "ProdTaken"
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            scaler = StandardScaler()
+
+            input_feature_train_arr=scaler.fit_transform(input_feature_train_df)
+            input_feature_test_arr=scaler.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
